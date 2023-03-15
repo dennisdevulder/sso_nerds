db6d9c5a015fd20ffa047145ce415b889275813e
Dennis de Vulder <dennisdevulder@Denniss-MacBook-Pro.local>
2023-03-15 15:14:08 +0100

    Remove redundant show check

--- spec/api/v1/data_controller_spec.rb
+++ spec/api/v1/data_controller_spec.rb
@@ -5,27 +5,6 @@ describe "api/v1/data", :type => :api do
   let(:error) {"Invalid OAuth Request"}
   let(:token) {Oauth2Token.create(:client_application => client_applications(:one), :user=>users(:aaron)).token}
 
-  context "invalid token" do
-
-    context "show" do
-
-      let(:url) { "api/v1/data" }
-      it "JSON" do
-        get "#{url}.json"
-        last_response.body.should eql(error)
-        last_response.status.should eql(401)
-      end
-
-      it "XML" do
-        get "#{url}.xml"
-        last_response.body.should eql(error)
-        last_response.status.should eql(401)
-      end
-
-    end
-
-  end
-
   context "valid token" do
 
     context "show" do
